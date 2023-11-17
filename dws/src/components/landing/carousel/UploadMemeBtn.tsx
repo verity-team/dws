6bbb2e6dff964da1cd7f84b1851484acd2509c36
Hoang Tien Minh <htminh.1001@gmail.com>
2023-11-17 15:26:44 +0700

    feat: add upload meme functionalities

--- dws/src/components/landing/carousel/UploadMemeBtn.tsx
+++ dws/src/components/landing/carousel/UploadMemeBtn.tsx
@@ -2,11 +2,24 @@
 
 import TextButton from "@/components/common/TextButton";
 import { Dialog, DialogContent, DialogTitle } from "@mui/material";
-import { useCallback, useState } from "react";
+import { ChangeEvent, FormEvent, useCallback, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
+import { useForm } from "react-hook-form";
+import { Maybe } from "@/utils/types";
+import Image from "next/image";
+
+interface UploadMemeFormData {
+  caption: string;
+  meme: File;
+}
 
 const UploadMemeButton = () => {
   const [uploadFormOpen, setUploadFormOpen] = useState(false);
+
+  const [currentMeme, setCurrentMeme] = useState<string>("");
+
+  const { register, handleSubmit } = useForm<UploadMemeFormData>();
 
   const handleOpenUploadForm = useCallback(() => {
     setUploadFormOpen(true);
@@ -15,6 +28,22 @@ const UploadMemeButton = () => {
   const handleCloseUploadForm = useCallback(() => {
     setUploadFormOpen(false);
   }, []);
+
+  const handleMemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newMeme = event.target.files?.item(0);
+    if (newMeme == null) {
+      return;
+    }
+
+    const newMemeUrl = URL.createObjectURL(newMeme);
+    setCurrentMeme(newMemeUrl);
+  };
+
+  const handleMemeRemove = () => {
+    setCurrentMeme("");
+  };
+
+  const handleUploadMeme = (data: UploadMemeFormData) => {};
 
   return (
     <div className="flex justify-center items-center">
@@ -30,24 +59,47 @@ const UploadMemeButton = () => {
         fullWidth
       >
         <DialogContent>
-          <form>
+          <form onSubmit={handleSubmit(handleUploadMeme)}>
             <div className="flex justify-center items-center text-2xl mb-4">
               Upload your meme
             </div>
-            <div className="mt-2 mb-4">
-              <label htmlFor="meme">
-                <div className="p-16 flex flex-col items-center justify-center border-2 border-black rounded-lg cursor-pointer">
-                  <AddIcon fontSize="medium" />
-                  <div>Add your meme here</div>
-                </div>
-              </label>
-              <input
-                type="file"
-                accept="image/jpeg, image/png, image/gif"
-                id="meme"
-                name="meme"
-                className="hidden"
-              />
+            <div className="mt-2 mb-4 relative">
+              {!!currentMeme ? (
+                <>
+                  <button
+                    onClick={handleMemeRemove}
+                    className="absolute top-2 right-2 rounded-full bg-gray-800 p-1"
+                  >
+                    <CloseIcon fontSize="medium" htmlColor="white" />
+                  </button>
+                  <Image
+                    src={currentMeme}
+                    alt="meme"
+                    width={100}
+                    height={100}
+                    className="w-full"
+                  />
+                </>
+              ) : (
+                <>
+                  <label htmlFor="meme">
+                    <div className="p-16 flex flex-col items-center justify-center border-2 border-black rounded-lg cursor-pointer">
+                      <AddIcon fontSize="medium" />
+                      <div>Add your meme here</div>
+                    </div>
+                  </label>
+                  <input
+                    type="file"
+                    accept="image/jpeg, image/png, image/gif"
+                    id="meme"
+                    className="hidden"
+                    {...register("meme", {
+                      required: true,
+                    })}
+                    onChange={handleMemeChange}
+                  />
+                </>
+              )}
             </div>
             <div className="my-2">
               <label htmlFor="caption" className="text-xl">
