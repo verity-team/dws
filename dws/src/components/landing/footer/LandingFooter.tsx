65e46b4aca7d324db5d1d4df448db85fedba3be7
Hoang Tien Minh <htminh.1001@gmail.com>
2024-01-06 00:23:04 +0700

    fix: migrate landing footer to desktop view

--- dws/src/components/landing/footer/LandingFooter.tsx
+++ dws/src/components/landing/footer/LandingFooter.tsx
@@ -1,4 +1,6 @@
 import DiscordIcon from "@/components/icons/DiscordIcon";
+import TelegramIcon from "@/components/icons/TelegramIcon";
+import XIcon from "@/components/icons/XIcon";
 import { InstagramIcon, SendIcon, TwitterIcon } from "lucide-react";
 import Image from "next/image";
 
@@ -6,41 +8,51 @@ const LandingFooter = () => {
   return (
     <>
       <div className="mx-8">
-        <div>
-          <h1 className="text-3xl italic tracking-wide inline-block break-words">
-            Join the the fun on <span className="text-cred">Telegram</span>
-          </h1>
-        </div>
-        <button
-          type="button"
-          className="mt-4 px-6 py-2 bg-cred text-white rounded-3xl border-4 border-black text-2xl tracking-wide uppercase"
-        >
-          <div className="flex items-center justify-center space-x-2">
-            <p>Join Telegram</p>
-            <SendIcon />
+        <div className="md:flex md:items-center md:justify-between">
+          <div>
+            <div>
+              <h1 className="text-3xl italic tracking-wide inline-block break-words md:text-5xl">
+                Join the the fun on <span className="text-cred">Telegram</span>
+              </h1>
+            </div>
+            <button
+              type="button"
+              className="mt-4 px-6 py-2 bg-cred text-white rounded-3xl border-4 border-black text-2xl tracking-wide uppercase"
+            >
+              <div className="flex items-center justify-center space-x-2">
+                <p>Join Telegram</p>
+                <SendIcon />
+              </div>
+            </button>
           </div>
-        </button>
-        <div className="relative w-full h-[30vh]">
-          <Image
-            src="/images/wojak.png"
-            alt="soyboy"
-            fill
-            sizes="100vw"
-            className="object-contain overflow-hidden"
-          />
+          <div className="relative md:flex md:items-center md:justify-center">
+            <Image
+              src="/images/wojak.png"
+              alt="soyboy"
+              width={300}
+              height={300}
+              className="object-contain overflow-hidden"
+            />
+          </div>
         </div>
-        <div className="mt-8 flex items-center justify-center">
-          <Image
-            src="/images/logo.png"
-            alt="eye of truth"
-            width={48}
-            height={0}
-            className="h-auto object-contain"
-          />
-          <div className="text-xl uppercase">Truth memes</div>
+        <div className="my-8 md:flex md:items-center md:justify-between">
+          <div className="flex items-center justify-center">
+            <Image
+              src="/images/logo.png"
+              alt="eye of truth"
+              width={48}
+              height={0}
+              className="h-auto object-contain"
+            />
+            <div className="text-xl uppercase">Truth memes</div>
+          </div>
+          <div className="flex items-center justify-center space-x-4">
+            <XIcon width={48} height={48} fill="#000" stroke="#000" />
+            <TelegramIcon width={48} height={48} />
+          </div>
         </div>
       </div>
-      <div className="w-full grid grid-cols-4 mt-8">
+      {/* <div className="w-full grid grid-cols-4 mt-8">
         <div className="border border-black flex flex-col items-center justify-center p-4 bg-corange cursor-pointer hover:bg-orange-400">
           <TwitterIcon />
           <div className="mt-1">Twitter</div>
@@ -57,7 +69,7 @@ const LandingFooter = () => {
           <InstagramIcon />
           <div>Instagram</div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
