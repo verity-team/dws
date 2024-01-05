2884b68c41b6cd7c5de36aed5ba52c41fbaae3c3
Hoang Tien Minh <htminh.1001@gmail.com>
2024-01-06 00:00:23 +0700

    fix: migrate story banner section to desktop view

--- dws/src/components/landing/banner/StoryBanner.tsx
+++ dws/src/components/landing/banner/StoryBanner.tsx
@@ -4,23 +4,17 @@ import { BookOpenText } from "lucide-react";
 const StoryBanner = () => {
   return (
     <section className="mx-8">
-      <div>
-        <h1 className="text-3xl italic tracking-wide inline-block break-words mt-2">
-          Uniting the <span className="text-cred">Truth</span> movements
-        </h1>
-      </div>
-      <div>
-        <h2 className="text-2xl tracking-wide inline-block break-words mt-2">
-          Media is failing, <span className="text-cred">Memes</span> are
-          winning; but we can only truly win together.
-        </h2>
-      </div>
-      <div className="mt-2">
-        <h2 className="text-2xl tracking-wide inline-block break-words mt-1">
-          Let&apos;s join forces!
-        </h2>
-      </div>
-      <div className="mt-4 space-y-4">
+      <h1 className="text-3xl italic tracking-wide inline-block break-words mt-2 md:text-5xl md:mt-8">
+        Uniting the <span className="text-cred">Truth</span> movements
+      </h1>
+      <h2 className="text-2xl tracking-wide inline-block break-words mt-2 md:text-4xl md:mt-8">
+        Media is failing, <span className="text-cred">Memes</span> are winning;
+        but we can only truly win together.
+      </h2>
+      <h2 className="mt-1 text-2xl tracking-wide inline-block break-words md:text-4xl md:mt-4">
+        Let&apos;s join forces!
+      </h2>
+      <div className="mt-4 space-y-4 md:mt-8">
         <button
           type="button"
           className="px-6 py-2 bg-cred text-white rounded-3xl border-4 border-black text-2xl tracking-wide uppercase"
@@ -31,7 +25,7 @@ const StoryBanner = () => {
           </div>
         </button>
       </div>
-      <div className="relative w-full h-[30vh] my-8 p-2 box-border border-4 border-black bg-white">
+      <div className="relative w-full h-[30vh] my-8 p-2 box-border border-8 border-black rounded-lg bg-white md:h-[70vh]">
         <div className="w-full h-full relative p-2 border-4 border-black">
           <Image
             src="/images/banner2.gif"
