3707dcd0df6b06ede7cae6104645483dcc841c50
Hoang Tien Minh <htminh.1001@gmail.com>
2024-01-07 12:23:19 +0700

    fix: auto clear current nonce when the current nonce expired

--- dws/src/hooks/galactica/account/useNonce.tsx
+++ dws/src/hooks/galactica/account/useNonce.tsx
@@ -1,10 +1,27 @@
 import { requestNonce } from "@/api/galactica/account/account";
 import { NonceInfo } from "@/api/galactica/account/account.type";
 import { Maybe } from "@/utils";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useNonce = () => {
   const [nonce, setNonce] = useState<Maybe<NonceInfo>>(null);
+
+  useEffect(() => {
+    if (nonce == null) {
+      return;
+    }
+
+    // Invalidat nonce when nonce reached its expiration time
+    const exp = new Date(nonce.expirationTime);
+    const timeToExpMs = exp.getTime() - Date.now();
+    const timerId = setTimeout(() => {
+      setNonce(null);
+    }, timeToExpMs);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [nonce]);
 
   const getNonce = useCallback(async (): Promise<Maybe<NonceInfo>> => {
     console.log("Current nonce");
