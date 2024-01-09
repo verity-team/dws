101a319045b43c6d89d3ab58a0c9a46359d52f29
Hoang Tien Minh <htminh.1001@gmail.com>
2024-01-10 00:38:17 +0700

    fix: enable user donation stat hook

--- dws/src/components/ico/stats/user/UserDonationStat.tsx
+++ dws/src/components/ico/stats/user/UserDonationStat.tsx
@@ -10,56 +10,56 @@ import { UserDonationData } from "@/api/dws/user/user.type";
 const UserDonationStat = () => {
   const userWallet = useContext(Wallet);
 
-  // const { data: userDonationData, error } = useUserDonationData(
-  //   userWallet.wallet
-  // );
+  const { data: userDonationData, error } = useUserDonationData(
+    userWallet.wallet
+  );
 
-  const userDonationData: UserDonationData = {
-    donations: [
-      {
-        amount: "1000",
-        asset: "USDT",
-        price: "1",
-        status: "confirmed",
-        ts: "2024-01-06T14:21:36.504Z",
-        tokens: "20000",
-        tx_hash:
-          "0x558c8b86f75b0149ddca028a44a302eebe47c032e8ca8834f07aa6f1c5ae3fac",
-        usd_amount: "1000",
-      },
-      {
-        amount: "3",
-        asset: "ETH",
-        price: "1",
-        status: "confirmed",
-        ts: "2024-01-06T14:21:36.504Z",
-        tokens: "20000",
-        tx_hash:
-          "0x558c8b86f75b0149ddca028a44a302eebe47c032e8ca8834f07aa6f1c5ae3fac",
-        usd_amount: "1000",
-      },
-      {
-        amount: "1000",
-        asset: "USDT",
-        price: "1",
-        status: "confirmed",
-        ts: "2024-01-06T14:21:36.504Z",
-        tokens: "20000",
-        tx_hash:
-          "0x558c8b86f75b0149ddca028a44a302eebe47c032e8ca8834f07aa6f1c5ae3fac",
-        usd_amount: "1000",
-      },
-    ],
-    user_data: {
-      total: "31415",
-      tokens: "9880000",
-      staked: "979999",
-      reward: "979",
-      status: "staking",
-      ts: "2023-10-05T09:23:10+00:00",
-      affiliate_code: "GezIkgeubrOkiOv4",
-    },
-  };
+  // const userDonationData: UserDonationData = {
+  //   donations: [
+  //     {
+  //       amount: "1000",
+  //       asset: "USDT",
+  //       price: "1",
+  //       status: "confirmed",
+  //       ts: "2024-01-06T14:21:36.504Z",
+  //       tokens: "20000",
+  //       tx_hash:
+  //         "0x558c8b86f75b0149ddca028a44a302eebe47c032e8ca8834f07aa6f1c5ae3fac",
+  //       usd_amount: "1000",
+  //     },
+  //     {
+  //       amount: "3",
+  //       asset: "ETH",
+  //       price: "1",
+  //       status: "confirmed",
+  //       ts: "2024-01-06T14:21:36.504Z",
+  //       tokens: "20000",
+  //       tx_hash:
+  //         "0x558c8b86f75b0149ddca028a44a302eebe47c032e8ca8834f07aa6f1c5ae3fac",
+  //       usd_amount: "1000",
+  //     },
+  //     {
+  //       amount: "1000",
+  //       asset: "USDT",
+  //       price: "1",
+  //       status: "confirmed",
+  //       ts: "2024-01-06T14:21:36.504Z",
+  //       tokens: "20000",
+  //       tx_hash:
+  //         "0x558c8b86f75b0149ddca028a44a302eebe47c032e8ca8834f07aa6f1c5ae3fac",
+  //       usd_amount: "1000",
+  //     },
+  //   ],
+  //   user_data: {
+  //     total: "31415",
+  //     tokens: "9880000",
+  //     staked: "979999",
+  //     reward: "979",
+  //     status: "staking",
+  //     ts: "2023-10-05T09:23:10+00:00",
+  //     affiliate_code: "GezIkgeubrOkiOv4",
+  //   },
+  // };
 
   // User have not connected, or there are no data on this user
   // Simply skip user stats rendering
