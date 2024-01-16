62bf3ec7591559adb92b6ab921a8db1521f0e91d
Hoang Tien Minh <htminh.1001@gmail.com>
2024-01-16 23:17:20 +0700

    fix: update nextjs config and rebuild image for production

--- dws/next.config.js
+++ dws/next.config.js
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpack: (config) => {
-  //   config.module.rules?.push({
-  //     test: /src\/app\/api/,
-  //     loader: "ignore-loader",
-  //   });
-  //   return config;
-  // },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-  images: {
-    domains: ["localhost"],
-  },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    outputFileTracingIncludes: {
+      "/image": ["./node_modules/@img/**"],
+    },
   },
   output: "standalone",
 };
