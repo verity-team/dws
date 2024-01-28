7a7c49bb5b6cefe704dd43eff6ef30e1f0e2a008
Hoang Tien Minh <htminh.1001@gmail.com>
2024-01-28 17:04:53 +0700

    fix: add new global store to control global user access token

---
+++ dws/src/hooks/store/useAccountId.tsx
@@ -0,0 +1,36 @@
+import { requestAccessTokenVerification } from "@/api/galactica/admin/admin";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface AccountId {
+  accessToken: string;
+  getAccessToken: () => string;
+  setAccessToken: (newAccessToken: string) => void;
+  verifyAccessToken: () => Promise<boolean>;
+}
+
+const useAccountId = create<AccountId>()(
+  persist(
+    (set, get) => ({
+      accessToken: "",
+      getAccessToken: () => get().accessToken,
+      setAccessToken: (newAccessToken) =>
+        set(() => ({ accessToken: newAccessToken })),
+      verifyAccessToken: async () => {
+        const currentAccessToken = get().accessToken;
+        if (!currentAccessToken) {
+          return false;
+        }
+
+        const isValid =
+          await requestAccessTokenVerification(currentAccessToken);
+        return isValid;
+      },
+    }),
+    {
+      name: "dws-account-id",
+    }
+  )
+);
+
+export default useAccountId;
